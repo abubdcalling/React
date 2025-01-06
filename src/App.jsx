11121c78e9ad9619a0eee6d9d486f8722f60be03
abu-said <abubdcalling@gmail.com>
2025-01-06 17:14:43 +0600

    loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  let marks = 80;
+  const city = ["dhaka", "london", "delhi", "kolka"];
 
-  return <>{marks > 80 ? <h1>ok</h1> : <h1>ok</h1>}</>;
+  return (
+    <div>
+      <ol>
+        {city.map((item, i) => {
+          return <li key={i.toString()}>{item}</li>;
+        })}
+      </ol>
+    </div>
+  );
 };
 
 export default App;
