7c1a229092befc79ffc957affb055408fb3130bf
abu-said <abubdcalling@gmail.com>
2025-01-06 17:28:51 +0600

    rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import React, { useState } from "react";
 const App = () => {
   const [count, setCount] = useState(0);
 
-  const city = ["dhaka", "london", "delhi", "kolka"];
+  let status = true;
 
   return (
     <div>
-      <ol>
-        {city.map((item, i) => {
-          return <li key={i.toString()}>{item}</li>;
-        })}
-      </ol>
+      <h1>Login status</h1>
+
+      {}
     </div>
   );
 };
