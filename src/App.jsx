bed65e7b3db0b0c45718c21c2a420b73228f0846
abu-said <abubdcalling@gmail.com>
2025-01-07 10:53:39 +0600

    hooks with usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 const App = () => {
-  let headline = useRef();
+  const [myob, setOb] = useState({
+    key1: "key 1 value 1",
+    key2: "key 2 value 2",
+    key3: "key 3 value 3",
+  });
 
   const change = () => {
-    headline.current.innerText = "hello useref";
+    setOb({
+      key1: "new key 1 value 1",
+      key2: "new key 2 value 2",
+      key3: "new key 3 value 3",
+    });
   };
 
   return (
     <div>
-      <h1 ref={headline}>Here shows data</h1>
+      <h1>{myob.key3}</h1>
       <button onClick={change}>click</button>
     </div>
   );
